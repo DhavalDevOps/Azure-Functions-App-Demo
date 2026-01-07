5ffa1937b3f4235d10874b192d77d6e8dbca8d65
DhavalDevOps <dhaval.chhayla.devops@gmail.com>
2026-01-07 12:54:04 +0530

    Updated the version

--- Functions/HttpHealth/__init__.py
+++ Functions/HttpHealth/__init__.py
@@ -5,7 +5,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     response = {
         "status": "UP",
         "service": "azure-functions-demo",
-        "version": "1.0.0"
+        "version": "1.1.0"
     }
 
     return func.HttpResponse(
